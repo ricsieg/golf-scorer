ed9d40f248166b4cf53bb1db897ab588efec3565
ricsieg <ricsieg@gmail.com>
2026-09-09 14:01:48 +1000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-// Golf Mob — clears old cache and unregisters
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-  );
-});
